a9e58804e2d29e2b71fece9f22090c3b9b1e8a62
john681611 <john681611@hotmail.com>
2020-01-25 12:53:40 +0000

    1,2 java passing 3 WIP

--- src/main/java/CodeTest.java
+++ src/main/java/CodeTest.java
@@ -1,4 +1,10 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class CodeTest {
     public static void main(String[] args) {
@@ -6,18 +12,26 @@ public class CodeTest {
     }
 
     public static String[] reverseArray(String[] input) {
-        // add code here
-        return input;
+        ArrayList<String> list = new ArrayList<String>(Arrays.asList(input));
+        Collections.reverse(list);
+        return list.toArray(input);
     }
 
     public static String[] uppercaseArray(String[] input) {
-        // add code here
-        return input;
+        ArrayList<String> list = new ArrayList<String>(Arrays.asList(input));
+        return list.stream().map(s -> s.toUpperCase()).collect(Collectors.toList()).toArray(input);
+    }
+
+    public static Map<String, Integer> findWordCountAll(String text) {
+        ArrayList<String> list = new ArrayList<String>(Arrays.asList(text.split(" ")));
+        Map<String,Integer> map = new HashMap<String,Integer>();
+        list.stream().map(s -> map.get(s) == null ? map.put(s, 1) : map.put(s, map.get(s) + 1));
+        return map;
     }
 
     public static int findWordCount(String text, String wordToFind) {
-        // add code here
-        return 0;
+        Map<String,Integer> map = findWordCountAll(text);
+        return map.get(wordToFind);
     }
 
     public static Function<Integer,Integer> composeU(Function<Integer,Integer> f1, Function<Integer,Integer> f2){
--- src/test/java/CodeTestSpec.java
+++ src/test/java/CodeTestSpec.java
@@ -1,5 +1,10 @@
 import org.junit.Test;
 import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /*
  *   Please code the tests in the format of reverseArray_returnsExpectedResult. This is an example of how we write our tests at Cardano.
@@ -25,13 +30,45 @@ public class CodeTestSpec {
 
     @Test
     public void uppercaseArray_returnsExpectedResult() {
+               // arrange
+               final String[] EXPECTED = {"X", "Y", "Z"};
 
+               // act
+               final String[] actual = CodeTest.uppercaseArray(new String[] {"x", "y", "z"});
+       
+               // assert
+               assertArrayEquals(EXPECTED, actual);
+    }
+
+    @Test
+    public void findWordCountAllWords_returnsExpectedResult() {
+        // arrange
+        final Map<String,Integer> EXPECTED = new HashMap<String,Integer>();
+        EXPECTED.put("the", 2);
+        EXPECTED.put("cat", 1);
+        EXPECTED.put("jumped", 1);
+        EXPECTED.put("over", 1);
+        EXPECTED.put("mat", 1);
+
+        // act
+        final Map<String,Integer> actual = CodeTest.findWordCountAll("the cat jumped over the mat");
+
+        // assert
+        assertTrue(EXPECTED.equals(actual));
     }
 
     @Test
     public void findWordCount_returnsExpectedResult() {
+        // arrange
+        final int EXPECTED = 2;
 
+        // act
+        final int actual = CodeTest.findWordCount("the cat jumped over the mat", "the");
+
+        // assert
+        assertEquals(EXPECTED, actual);
     }
+
 
     @Test
     public void composeU_returnsExpectedResult() {
